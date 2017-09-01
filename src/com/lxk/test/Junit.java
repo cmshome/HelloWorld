4712b5ea40d77734ccce5d32ab3b0c9a54d719c5
cmshome <cmshome@163.com>
2017-09-01 10:54:06 +0800

    junit测试

---
+++ src/com/lxk/test/Junit.java
@@ -0,0 +1,13 @@
+package com.lxk.test;
+
+import org.junit.Test;
+
+/**
+ * Created by lxk on 2017/8/29
+ */
+public class Junit {
+    @Test
+    public void test(){
+        System.out.println("sss");
+    }
+}
