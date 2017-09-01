cfd04b85da411d8250871ee2404056b8256866db
cmshome <cmshome@163.com>
2017-09-01 16:01:18 +0800

    junit测试

--- src/com/lxk/test/Junit.java
+++ src/com/lxk/test/Junit.java
@@ -6,6 +6,11 @@ import org.junit.Test;
  * Created by lxk on 2017/8/29
  */
 public class Junit {
+
+    public static void main(String[] args) {
+        System.out.println("sssssssssssssss");
+    }
+
     @Test
     public void test(){
         System.out.println("sss");
