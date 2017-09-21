c95f885bc7065e0f7d960d89898e94179f69416d
cmshome <cmshome@163.com>
2017-09-21 11:36:21 +0800

    indexOf四种重载的测试

--- src/com/lxk/commonTest/StringTest.java
+++ src/com/lxk/commonTest/StringTest.java
@@ -42,6 +42,25 @@ public class StringTest {
      * 使用indexOf()来拆分字符串：D:\Android\sdk\add-ons
      */
     private static void testIndexOf() {
+        String string = "aaa456ac";
+
+        //查找指定字符是在字符串中的下标。在则返回所在字符串下标；不在则返回-1.
+        System.out.println(string.indexOf("b"));//indexOf(String str)；返回结果：-1，"b"不存在
+
+        // 从第四个字符位置开始往后继续查找，包含当前位置
+        System.out.println(string.indexOf("a",3));//indexOf(String str, int fromIndex)；返回结果：6
+
+        //（与之前的差别：上面的参数是 String 类型，下面的参数是 int 类型）参考数据：a-97,b-98,c-99
+
+        // 从头开始查找是否存在指定的字符
+        System.out.println(string.indexOf(99));//indexOf(int ch)；返回结果：7
+        System.out.println(string.indexOf('c'));//indexOf(int ch)；返回结果：7
+
+        //从fromIndex查找ch，这个是字符型变量，不是字符串。字符a对应的数字就是97。
+        System.out.println(string.indexOf(97,3));//indexOf(int ch, int fromIndex)；返回结果：6
+        System.out.println(string.indexOf('a',3));//indexOf(int ch, int fromIndex)；返回结果：6
+
+        //这个就是灵活运用String类提供的方法，拆分提供的字符串。
         String s = "D:\\Android\\sdk\\add-ons";
         System.out.println(s);
         while (s.lastIndexOf("\\") > 0) {
