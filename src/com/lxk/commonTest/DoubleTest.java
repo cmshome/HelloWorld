3bf58bae396873adb71a02189f3048c82848702f
cmshome <cmshome@163.com>
2017-09-27 14:59:22 +0800

    float和double精确计算

--- src/com/lxk/commonTest/DoubleTest.java
+++ src/com/lxk/commonTest/DoubleTest.java
@@ -1,25 +1,46 @@
 package com.lxk.commonTest;
 
+import com.lxk.util.DoubleUtil;
 import com.lxk.util.PrintUtil;
+
+import java.text.DecimalFormat;
 
 /**
  * float和double只能用来做科学计算或者是工程计算.
  * 在商业计算中我们要用 java.math.BigDecimal
- *
+ * <p>
  * Created by lxk on 2017/9/27
  */
 public class DoubleTest {
     public static void main(String[] args) {
         testDouble();
+        PrintUtil.divideLine();
+        testDoubleExact();
     }
 
+    /**
+     * double的一些计算奇葩现象，试验一把，就印象深刻啦。
+     */
     private static void testDouble() {
         Double d = 0.81d;
         System.out.println(d);
         PrintUtil.divideLine();
-        System.out.println(0.05 + 0.01);//0.060000000000000005
-        System.out.println(1.0 - 0.42);//0.5800000000000001
-        System.out.println(4.015 * 100);//401.49999999999994
-        System.out.println(123.3 / 100);//1.2329999999999999
+        System.out.println("0.05 + 0.01 = " + (0.05 + 0.01));//0.060000000000000005
+        System.out.println("1.0 - 0.42 = " + (1.0 - 0.42));//0.5800000000000001
+        System.out.println("4.015 * 100 = " + (4.015 * 100));//401.49999999999994
+        System.out.println("123.3 / 100 = " + (123.3 / 100));//1.2329999999999999
+        System.out.println(new DecimalFormat("0.00").format(4.025d));//4.03 四舍五入
+    }
+
+    /**
+     * 精确计算
+     */
+    private static void testDoubleExact() {
+        System.out.println("0.05 + 0.01 = " + DoubleUtil.add(0.05, 0.01));
+        System.out.println("1.0 - 0.42 = " + DoubleUtil.sub(1.0, 0.42));
+        System.out.println("4.015 * 100 = " + DoubleUtil.mul(4.015, 100d));
+        System.out.println("123.3 / 100 = " + DoubleUtil.divide(123.3, 100d));//保留两位
+        System.out.println("123.3 / 100 = " + DoubleUtil.divide(123.3, 100d, 3));//保留三位
+        System.out.println(DoubleUtil.round(4.025d, 2));
     }
 }
