7dab0852b438ff134c928647044a8e468e78cb52
cmshome <cmshome@163.com>
2017-11-07 11:49:11 +0800

    悲哀，单词拼写错误，丢人啊。

--- src/com/lxk/fileTest/characterFile/WriterTest.java
+++ src/com/lxk/fileTest/characterFile/WriterTest.java
@@ -3,7 +3,7 @@ package com.lxk.fileTest.characterFile;
 /**
  * @author lxk on 2016/11/19
  */
-public class WtiterTest {
+public class WriterTest {
     public static void main(String[] args) {
 
     }
