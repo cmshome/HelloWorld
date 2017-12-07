e65c503666a04d965c02bfd981feaeebba2db523
cmshome <cmshome@163.com>
2017-12-07 16:13:45 +0800

    日期时间的格式化测试

--- src/com/lxk/commonTest/TimeTest.java
+++ src/com/lxk/commonTest/TimeTest.java
@@ -10,6 +10,7 @@ import java.util.Date;
 /**
  * 时间相关的测试
  * <p>
+ *
  * @author lxk on 2017/2/22
  */
 public class TimeTest {
@@ -21,16 +22,36 @@ public class TimeTest {
         //System.out.println(t.toString());
         //System.out.println("\r<br> 执行耗时 : " + (System.currentTimeMillis() - a) / 1000f + " 秒 ");
         //testCalendar();
-        //testStringToDate();
-        turnLongToDate();
+        testStringToDate();
+        //turnLongToDate();
+        //testGetTime();
     }
 
+    /**
+     * 编辑器检测代码，使用推荐方式初始化时间
+     * 运行结果没啥不一样啊
+     */
+    private static void testGetTime() {
+        // Positive example:
+        long a = System.currentTimeMillis();
+        // Negative example:
+        long b = new Date().getTime();
+        System.out.println("Positive example：" + a);
+        System.out.println("Negative example：" + b);
+    }
+
+    /**
+     * 时间戳 -> 格式化的时间字符串
+     */
     private static void turnLongToDate() {
         Date ping = new Date(1506743427564L);
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         System.out.println(format.format(ping));
     }
 
+    /**
+     * 字符串 -> 时间
+     */
     private static void testStringToDate() {
         String s = "2017-05-25";
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
@@ -42,7 +63,6 @@ public class TimeTest {
         }
         System.out.println(date);
     }
-
 
     /**
      * 查看 Calendar 获得年月日时分秒
@@ -68,7 +88,6 @@ public class TimeTest {
 
     }
 
-
     /**
      * 将Date类型的时间转成 2016-10-25 18:11:55 此格式
      * 时间戳转换为 2016-10-25 18:11:55 此格式
@@ -76,11 +95,13 @@ public class TimeTest {
      * @param ss Date类型的时间
      */
     private static void testTime(Date ss) {
-        System.out.println(ss);//打印一般的Date类型的时间
-        System.out.println(ss.getTime());//这个是把当前时间转换成秒数，即时间戳。
+        //打印一般的Date类型的时间
+        System.out.println(ss);
+        //这个是把当前时间转换成秒数，即时间戳。
+        System.out.println(ss.getTime());
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        String time = format.format(ss.getTime());//这个就是把时间戳经过处理得到期望格式的时间
+        //这个就是把时间戳经过处理得到期望格式的时间
+        String time = format.format(ss.getTime());
         System.out.println(time);
-
     }
 }
