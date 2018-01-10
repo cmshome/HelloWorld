bd05cc60f2ef487ab3a908d115bbb3c0f69c02e6
cmshome <cmshome@163.com>
2018-01-10 10:34:46 +0800

    i++ 不是原子操作，所以，多线程对同一个i++，即使对 i 使用 synchronized 操作，也不能保证线程安全。应当使用 AtomicInteger ，可保线程安全无忧。

--- src/com/lxk/threadTest/mianShiTest/staticAttribute/MyThread.java
+++ src/com/lxk/threadTest/mianShiTest/staticAttribute/MyThread.java
@@ -1,18 +1,21 @@
 package com.lxk.threadTest.mianShiTest.staticAttribute;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * @author lxk on 2017/11/17
  */
 public class MyThread implements Runnable {
 
-    volatile static Integer i = 0;
+    private static AtomicInteger i = new AtomicInteger(0);
 
     @Override
     public void run() {
         while (true) {
             synchronized (i) {
-                if (i < 100) {
-                    i++;
+                if (i.get() < 100) {
+                    //相当于i++
+                    i.getAndIncrement();
                     String currentThreadName = Thread.currentThread().getName();
                     System.out.println(currentThreadName + " i = " + i);
                 } else {
