3a6772df2a5dc8711a2851e9c6442a47993aa9f8
cmshome <cmshome@163.com>
2018-01-24 20:12:14 +0800

    jdk 1.8 从集合过滤元素的方法：removeIf

--- src/com/lxk/java8/RemoveIfTest.java
+++ src/com/lxk/java8/RemoveIfTest.java
@@ -1,7 +1,8 @@
 package com.lxk.java8;
 
-import com.lxk.util.CollectionUtil;
+import com.google.common.collect.Lists;
 
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -11,17 +12,55 @@ import java.util.List;
  */
 public class RemoveIfTest {
     public static void main(String[] args) {
-        removeIfTest();
+        //removeIfTest();
+        beforeRemove();
     }
 
     /**
-     * 删除集合中复合条件的成员，empty集合也可以
+     * 咱之前是怎么从集合中过滤掉不要的元素的
+     */
+    private static void beforeRemove() {
+        List<String> list = Lists.newArrayList("1", "12", "13", "14", "15", "0");
+        System.out.println("初始时：" + list.toString());
+
+        System.out.println("for i 循环删除元素");
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).contains("1")) {
+                list.remove(i);
+                //一般之前这么操作集合的时候，估计都是会忘记 i-- 的
+                i--;
+            }
+        }
+        System.out.println("过滤完：" + list.toString());
+
+        System.out.println("Iterator 迭代器 循环删除元素");
+        list = Lists.newArrayList("1", "12", "13", "14", "15", "0");
+        Iterator<String> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            if (iterator.next().contains("1")) {
+                iterator.remove();
+            }
+        }
+        System.out.println("过滤完：" + list.toString());
+        System.out.println("加强 for 循环  循环删除元素，直接异常。");
+        list = Lists.newArrayList("1", "12", "13", "14", "15", "0");
+        for (String s : list) {
+            if (s.contains("1")) {
+                list.remove(s);
+            }
+        }
+        System.out.println("过滤完：" + list.toString());
+    }
+
+    /**
+     * 删除集合中符合条件的成员，empty集合也可以，但是null就炸啦。
      */
     private static void removeIfTest() {
-        List<String> list = CollectionUtil.getArrayList();
-        System.out.println(list.size());
-        list.removeIf(s -> !s.equals("1"));
-        System.out.println(list.size());
-        System.out.println(list.toString());
+        List<String> list = Lists.newArrayList("1", "12", "13", "14", "15", "0");
+        System.out.println("初始时：" + list.toString());
+        list.removeIf(s -> s.contains("1"));
+        System.out.println("过滤完：" + list.toString());
+
+        list.forEach(System.out::println);
     }
 }
