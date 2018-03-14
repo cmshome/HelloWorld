07909bb22dd89caf24d58cd5008798df3cd547ac
cmshome <cmshome@163.com>
2018-03-14 17:26:11 +0800

    枚举使用继承来组织的测试。

---
+++ src/com/lxk/model/enumModel/CoffeeEnum.java
@@ -0,0 +1,11 @@
+package com.lxk.model.enumModel;
+
+/**
+ * @author lxk on 2018/3/14
+ */
+public enum CoffeeEnum implements Food {
+    BLACK_COFFEE,
+    DECAF_COFFEE,
+    LATTE,
+    CAPPUCCINO
+}
---
+++ src/com/lxk/model/enumModel/Food.java
@@ -0,0 +1,12 @@
+package com.lxk.model.enumModel;
+
+/**
+ * @author lxk on 2018/3/14
+ */
+public interface Food {
+    enum DessertEnum implements Food {
+        FRUIT,
+        CAKE,
+        GELATO
+    }
+}
