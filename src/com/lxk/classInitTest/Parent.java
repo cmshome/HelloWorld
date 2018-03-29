57ec69f0a67e56d891ea2f9c8757a94001ca715a
cmshome <cmshome@163.com>
2018-03-29 20:00:29 +0800

    类加载执行顺序

--- src/com/lxk/classInitTest/Parent.java
+++ src/com/lxk/classInitTest/Parent.java
@@ -7,11 +7,11 @@ package com.lxk.classInitTest;
  */
 public class Parent {
     static {
-        System.out.println("父类：静态代码块");
+        System.out.println("1 父类：静态代码块");
     }
 
     {
-        System.out.println("父类：构造代码块");
+        System.out.println("5 父类：构造代码块");
     }
 
     private static String staticStringInParent = initStaticStringInParent();
@@ -19,15 +19,15 @@ public class Parent {
     private String stringInParent = initStringInParent();
 
     public Parent() {
-        System.out.println("父类：构造方法");
+        System.out.println("7 父类：构造方法");
     }
 
     private static String initStaticStringInParent() {
-        System.out.println("父类：静态方法，被静态成员变量赋值调用。");
+        System.out.println("2 父类：静态方法，被静态成员变量赋值调用。");
         return "initStaticStringInParent";
     }
     private String initStringInParent() {
-        System.out.println("父类：普通成员方法，被普通成员变量赋值调用。");
+        System.out.println("6 父类：普通成员方法，被普通成员变量赋值调用。");
         return "initStringInParent";
     }
 
