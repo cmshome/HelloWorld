bda82817e9e602077c9baf0f9f38a7c16278965f
cmshome <cmshome@163.com>
2018-04-08 17:01:21 +0800

    注释更新

--- src/com/lxk/throwable/ErrorTest.java
+++ src/com/lxk/throwable/ErrorTest.java
@@ -16,9 +16,18 @@ public class ErrorTest {
 
     /**
      * 【堆】内存溢出
+     *  栈深度大于虚拟机所允许的深度，将抛出StackOverflowError异常.
+     *  在这之前要耗尽堆内存，就会出现OOM异常。
      */
     private static void outOfMemoryError() {
         new Test();
+        //下面这个反应太慢了
+        //List l = new LinkedList();
+        // Enter infinite loop which will add a String to the list: l on each
+        // iteration.
+        //do {
+        //    l.add(new String("Hello, World"));
+        //} while (true);
     }
 
     /**
