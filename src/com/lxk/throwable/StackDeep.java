6dc6598ab69a6fed5118e245e210790544587aff
cmshome <cmshome@163.com>
2018-04-16 18:51:37 +0800

    到达jvm栈深度时，抛栈溢出异常。

---
+++ src/com/lxk/throwable/StackDeep.java
@@ -0,0 +1,15 @@
+package com.lxk.throwable;
+
+/**
+ * 栈的深度
+ *
+ * @author lxk on 2018/4/16
+ */
+public class StackDeep {
+    public static void main(String[] args) {
+        //19337
+        new StackDeepTest().test();
+    }
+
+}
+
