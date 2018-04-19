3563a8caaf03e24eafc5c2230278415f1bd178d2
cmshome <cmshome@163.com>
2018-04-19 17:46:49 +0800

    泛型对象

--- src/com/lxk/model/Point.java
+++ src/com/lxk/model/Point.java
@@ -2,10 +2,12 @@ package com.lxk.model;
 
 /**
  * 坐标model 测试泛型用
- * <p>
+ * 希望泛型类型只能是某一部分类型，你会希望是Number或其子类类型。这个想法其实就是给泛型参数添加一个界限。
+ *
  * @author lxk on 2017/6/14
  */
 public class Point<T extends Number> {
+    //
     private T x;
     private T y;
 
