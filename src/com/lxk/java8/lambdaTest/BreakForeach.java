1b6e60213ace6585c3c748589b3b14738bde2738
请叫我大师兄 <cmshome@163.com>
2018-10-23 14:21:31 +0800

    Java8foreach循环的各种跳出循环的姿势

--- src/com/lxk/java8/lambdaTest/BreakForeach.java
+++ src/com/lxk/java8/lambdaTest/BreakForeach.java
@@ -2,6 +2,7 @@ package com.lxk.java8.lambdaTest;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * 跳出for循环
@@ -14,13 +15,25 @@ public class BreakForeach {
         List<String> list = Arrays.asList("123", "12345", "1234", "4321", "1234567", "5678");
         continueForeachJava8(max, list);
         System.out.println();
+        breakForeachJava8(max, list);
+        System.out.println();
         continueFor(max, list);
         System.out.println();
         breakFor(max, list);
         System.out.println();
         breakManyFor(max, list);
-        System.out.println();
-        breakForeachJava8(max, list);
+    }
+
+    /**
+     * Java8跳过一次foreach循环，然后继续执行。
+     */
+    private static void continueForeachJava8(int max, List<String> list) {
+        list.forEach(s -> {
+            if (s.length() >= max) {
+                return;
+            }
+            System.out.println(s);
+        });
     }
 
     /**
@@ -28,11 +41,52 @@ public class BreakForeach {
      * 还这没找到，怎么跳出
      */
     private static void breakForeachJava8(int max, List<String> list) {
-        // TODO: 2018/10/23
+        //这么做是不对的
+        //try {
+        //    list.forEach(s -> {
+        //        if (s.length() >= max) {
+        //            throw new Exception();
+        //        }
+        //        System.out.println(s);
+        //    });
+        //} catch (Exception e) {
+        //    System.out.println(e.getMessage());
+        //}
+
+        //看需求，建议使用如下之一。
+        Optional<String> result = list.stream().filter(s -> s.length() >= max).findFirst();
+        System.out.println(result.orElse(null));
+
+        boolean lxk = list.stream().anyMatch(s -> s.length() >= max);
+        System.out.println(lxk);
     }
 
     /**
-     * break lxk 是直接跳出多层for循环，不再继续执行了。
+     * continue 跳过本次循环，继续执行。
+     */
+    private static void continueFor(int max, List<String> list) {
+        for (String s : list) {
+            if (s.length() >= max) {
+                continue;
+            }
+            System.out.println(s);
+        }
+    }
+
+    /**
+     * break 是直接跳出for循环，不再继续执行for循环到代码了。
+     */
+    private static void breakFor(int max, List<String> list) {
+        for (String s : list) {
+            if (s.length() >= max) {
+                break;
+            }
+            System.out.println(s);
+        }
+    }
+
+    /**
+     * break lxk 是直接跳出多层for循环，不再继续执行for循环到代码了。
      */
     private static void breakManyFor(int max, List<String> list) {
         lxk:
@@ -50,39 +104,4 @@ public class BreakForeach {
         }
     }
 
-    /**
-     * break 是直接跳出for循环，不再继续执行了。
-     */
-    private static void breakFor(int max, List<String> list) {
-        for (String s : list) {
-            if (s.length() >= max) {
-                break;
-            }
-            System.out.println(s);
-        }
-    }
-
-    /**
-     * continue 跳过本次循环，继续执行。
-     */
-    private static void continueFor(int max, List<String> list) {
-        for (String s : list) {
-            if (s.length() >= max) {
-                continue;
-            }
-            System.out.println(s);
-        }
-    }
-
-    /**
-     * Java8跳过一次foreach循环的方式
-     */
-    private static void continueForeachJava8(int max, List<String> list) {
-        list.forEach(s -> {
-            if (s.length() >= max) {
-                return;
-            }
-            System.out.println(s);
-        });
-    }
 }
