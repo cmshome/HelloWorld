9de29aff97668bf55af1b09fc813ecf8e99b860f
请叫我大师兄 <cmshome@163.com>
2019-01-23 17:30:45 +0800

    Java的 多态

--- src/com/lxk/extendTest/ExtendTestMain.java
+++ src/com/lxk/extendTest/ExtendTestMain.java
@@ -1,6 +1,8 @@
 package com.lxk.extendTest;
 
 import com.lxk.model.Child;
+import com.lxk.model.Parent;
+import com.lxk.util.PrintUtil;
 
 /**
  * 测试继承中的super用法
@@ -19,5 +21,11 @@ public class ExtendTestMain {
         Child child = new Child();
         child.say();
         child.sayHello();
+
+        PrintUtil.divideLine();
+
+        //java 的多态，说的就是这个。
+        Parent parent = new Child();
+        parent.say();
     }
 }
