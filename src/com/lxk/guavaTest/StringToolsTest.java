aca3527c6183b37fbd497898f586925902e6be14
lxk <lixk@fusionskye.com>
2019-05-10 14:11:43 +0800

    1，获得字符数组：[A-Z,a-z,0-1] 2，获得一个包含 count 个字符串list，每个字符串有 eachLength 个字符

--- src/com/lxk/guavaTest/StringToolsTest.java
+++ src/com/lxk/guavaTest/StringToolsTest.java
@@ -2,9 +2,13 @@ package com.lxk.guavaTest;
 
 import com.google.common.base.Joiner;
 import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
 import com.google.common.primitives.Ints;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 
 /**
  * 测试 String处理相关的几个工具
@@ -17,8 +21,63 @@ public class StringToolsTest {
         commonNullOrEmpty(targetString);
         toolsNullOrEmpty(targetString);
         System.out.println(toolJoinerTest());
+
+        int count = 800;
+        int eachLength = 8;
+        List<String> randomStringList = getRandomStringList(count, eachLength);
+        System.out.println(randomStringList.toString());
     }
 
+    /**
+     * 获得一个包含 count 个字符串list，每个字符串有 eachLength 个字符
+     *
+     * @param count      返回多少个字符串的list
+     * @param eachLength 每个字符串几个字符
+     */
+    private static List<String> getRandomStringList(int count, int eachLength) {
+
+        List<Character> characters = getCharacterList();
+        List<String> strings = Lists.newArrayListWithExpectedSize(count);
+        for (int j = 0; j < count; j++) {
+            StringBuilder ss = new StringBuilder();
+            int i = 0;
+            while (i < eachLength) {
+                ss.append(characters.get(new Random().nextInt(characters.size())));
+                i++;
+            }
+            strings.add(ss.toString());
+        }
+        return strings;
+    }
+
+    /**
+     * 获得字符数组：[A-Z,a-z,0-1]
+     */
+    private static List<Character> getCharacterList() {
+        char a = 'a';
+        char z = 'z';
+        char A = 'A';
+        char Z = 'Z';
+        char _0 = '0';
+        char _9 = '9';
+
+        List<Character> characters = Lists.newArrayList();
+
+        while (A <= Z) {
+            characters.add(A);
+            A++;
+        }
+        while (a <= z) {
+            characters.add(a);
+            a++;
+        }
+        while (_0 <= _9) {
+            characters.add(_0);
+            _0++;
+        }
+        Collections.sort(characters);
+        return characters;
+    }
     /**
      * 获得要判断的目标字符串
      */
