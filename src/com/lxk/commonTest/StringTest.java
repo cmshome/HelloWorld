296da86164ecd37e9efb091ab3d6d738a559a0e6
lxk <lixk@fusionskye.com>
2019-06-05 10:25:28 +0800

    同样是处理字符串，不一样的写法，效率就是不一样，提醒写方法的时候要是考虑一下效率问题，是不是会好点儿呢。

--- src/com/lxk/commonTest/StringTest.java
+++ src/com/lxk/commonTest/StringTest.java
@@ -20,6 +20,9 @@ public class StringTest {
     private static final String HEL_;
     private static final String LO_;
 
+    private static final char DOUBLE_QUOTATION_MARKS = '"';
+    private static final char SINGLE_QUOTATION_MARK = '\'';
+
     static {
         HEL_ = "Hel";
         LO_ = "lo";
@@ -47,23 +50,120 @@ public class StringTest {
         //Arrays.stream(split).filter(s1 -> !"OR".equals(s1)).forEach(System.out::println);
         //Arrays.stream("sda.sda".split("\\.")).forEach(System.out::println);
 
-        testCleanString();
+        testEfficiency();
 
+    }
+
+    /**
+     * 测试下面三个处理字符串的效率问题
+     * cleanString ：cleanString2：cleanString3 = 32.9 : 23.9 : 30.2
+     * 可见 cleanString2 效率高。
+     */
+    private static void testEfficiency() {
+        while (true) {
+            testCleanString();
+            testCleanString2();
+            testCleanString3();
+        }
     }
 
     private static void testCleanString() {
         String s = "\"1234\"";
         cleanString(s);
+
         s = "'1234'";
         cleanString(s);
+
         s = "1234";
         cleanString(s);
+
         s = "123123'";
         cleanString(s);
-        s = "123123\'";
+
+        s = "12315623\'";
         cleanString(s);
+
         s = "123123\"\"";
         cleanString(s);
+    }
+
+    private static void testCleanString2() {
+        String s = "\"1234\"";
+        cleanString2(s);
+
+        s = "'1234'";
+        cleanString2(s);
+
+        s = "1234";
+        cleanString2(s);
+
+        s = "123123'";
+        cleanString2(s);
+
+        s = "12315623\'";
+        cleanString2(s);
+
+        s = "123123\"\"";
+        cleanString2(s);
+    }
+
+    private static void testCleanString3() {
+        String s = "\"1234\"";
+        cleanString3(s);
+
+        s = "'1234'";
+        cleanString3(s);
+
+        s = "1234";
+        cleanString3(s);
+
+        s = "123123'";
+        cleanString3(s);
+
+        s = "12315623\'";
+        cleanString3(s);
+
+        s = "123123\"\"";
+        cleanString3(s);
+    }
+
+    private static String cleanString3(String value) {
+        if ((value == null) || (value.isEmpty())) {
+            return null;
+        }
+        if ((value.startsWith("\"") && value.endsWith("\""))
+                || (value.startsWith("'") && value.endsWith("'"))) {
+            value = value.substring(1, value.length() - 1);
+        }
+        return value;
+    }
+
+    /**
+     * 相比下面的，判断次数是减少了，效率提高啦。
+     * 字符串如果包含了""或者''，去掉最外层的引号。
+     *
+     * @param value 值
+     * @return 字符串值
+     */
+    private static String cleanString2(String value) {
+        if ((value == null) || (value.isEmpty())) {
+            return null;
+        }
+        char firstChar = value.charAt(0);
+        if (firstChar == DOUBLE_QUOTATION_MARKS) {
+            int lastIndex = value.length() - 1;
+            char lastChar = value.charAt(lastIndex);
+            if (lastChar == DOUBLE_QUOTATION_MARKS) {
+                return value.substring(1, lastIndex);
+            }
+        } else if (firstChar == SINGLE_QUOTATION_MARK) {
+            int lastIndex = value.length() - 1;
+            char lastChar = value.charAt(lastIndex);
+            if (lastChar == SINGLE_QUOTATION_MARK) {
+                return value.substring(1, lastIndex);
+            }
+        }
+        return value;
     }
 
     /**
@@ -73,7 +173,6 @@ public class StringTest {
      * @return 字符串值
      */
     private static String cleanString(String value) {
-        System.out.println(value);
         if ((value == null) || (value.isEmpty())) {
             return null;
         }
@@ -88,8 +187,6 @@ public class StringTest {
         if (containDouble || containSingle) {
             value = value.substring(1, lastIndex);
         }
-        System.out.println(value);
-        System.out.println();
         return value;
     }
 
